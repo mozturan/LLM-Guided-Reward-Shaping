fdf54d286e66a646d08a8c5f0bcd62e7643eda4d
mozturan <mehmetozturan@proton.me>
2026-04-24 16:36:39 +0300

    llm generator tests

---
+++ generator/llm.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+# Load variables from .env into the environment
+load_dotenv()
+
+# Access the key
+api_key = os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    raise ValueError("API Key not found. Did you set it in the .env file?")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-3.1-flash-lite-preview",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
+
